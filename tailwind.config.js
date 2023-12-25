ae6b842f20ec22e4eadf1a4d49be3078ca7e94e7
naskriK <naskri.kamil@gmail.com>
2023-12-25 14:24:14 +0100

    Custom font
    Add custom font to tailwind.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ["Montserrat", "sans-serif"],
+        secondary: ["Fraunces", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
