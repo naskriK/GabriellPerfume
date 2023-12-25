08b8a17440f9a97dd46e4c55f23dc8afe40ec39d
naskriK <naskri.kamil@gmail.com>
2023-12-25 14:57:25 +0100

    Mobile Tailwind
    Styling components on mobile using tailwind, practice classes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ export default {
         primary: ["Montserrat", "sans-serif"],
         secondary: ["Fraunces", "sans-serif"],
       },
+      colors: {
+        primary: "hsl(158, 36%, 37%)",
+        background: "hsl(30, 38%, 92%)",
+        heading: "hsl(212, 21%, 14%)",
+        text: "hsl(228, 12%, 48%)",
+      },
     },
   },
   plugins: [],
